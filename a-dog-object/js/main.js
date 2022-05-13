0cf385ec107d5926690403910c8200b167d4eb68
AyobamiH <heryourbarme@live.com>
2022-05-13 14:49:12 +0100

    comments

--- a-dog-object/js/main.js
+++ a-dog-object/js/main.js
@@ -1,12 +1,16 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
 
-// get api endpoint
+//A function that gets the api endpoint 
 function showPic(){
     fetch('https://dog.ceo/api/breeds/image/random')
     
   .then(response => response.json())
+
+  // place the response data message in the src attrs of .imageshere class in the dom
   .then(data => document.querySelector('.imageshere').src = data.message);
 }
+
+// A button-click event Listener
 let click = document.querySelector('button').addEventListener('click', showPic)
 
 // let user choose the option of how many pictures to display
